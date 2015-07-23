40ef88aeedd59ff9a721e439d37960b93402aa0b
Kevin Sproles <aspire2x@gmail.com>
2015-07-22 22:24:13 -0500

    added /search with Algolia * which required added a bunch of browser dependencies: ** algoliaSearch, bootstrap, bootstrap-slider, and hogan

--- to_generate/js/app.js
+++ to_generate/js/app.js
@@ -1,4 +1,8 @@
+'use strict';
+
 var $ = require('jquery');
 var whatev = require('./whatever');
+var algolia = require('./algolia');
+var bootstrapSlider = require('bootstrap-slider');
 
 $('.haters').fadeOut();
