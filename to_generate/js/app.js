453b767906516569937fabee482fc3e9296cd1c7
Kevin Sproles <aspire2x@gmail.com>
2015-07-28 18:53:58 -0500

    * fixed trailing slash issue * fixed roboto font path

--- to_generate/js/app.js
+++ to_generate/js/app.js
@@ -6,7 +6,9 @@ var url = require('url');
 
 /////////////////////////////////////////////////
 // Algolia Config
-if (url.parse(location.href).pathname === '/search/') {
+var urlPathName = url.parse(location.href).pathname;
+urlPathName = stripTrailingSlash(urlPathName);
+if (urlPathName === '/search') {
   algolia.init();
   algolia.config.APPLICATION_ID = '7FI9T0IXZ5';
   algolia.config.SEARCH_ONLY_API_KEY = 'b998ea87dc6edcbdf733d2796f8eccf7';
@@ -15,7 +17,7 @@ if (url.parse(location.href).pathname === '/search/') {
     { name: 'fragments.product.flavour.value', title: 'flavor', disjunctive: true, sortFunction: algolia.sortByName, topListIfRefined: true },
     { name: 'fragments.product.name.value.text', title: 'name', disjunctive: true, sortFunction: algolia.sortByName, topListIfRefined: true }
   ];
-} else if (url.parse(location.href).pathname === '/examples/search-best-buy/') {
+} else if (urlPathName === '/examples/search-best-buy') {
   algolia.init();
   algolia.config.APPLICATION_ID = 'latency';
   algolia.config.SEARCH_ONLY_API_KEY = '6be0576ff61c053d5f9a3225e2a90f76';
@@ -32,3 +34,10 @@ if (url.parse(location.href).pathname === '/search/') {
 algolia.config.HITS_PER_PAGE = 10;
 algolia.config.MAX_VALUES_PER_FACET = 30;
 /////////////////////////////////////////////////
+
+function stripTrailingSlash(str) {
+    if(str.substr(-1) === '/') {
+        return str.substr(0, str.length - 1);
+    }
+    return str;
+}
